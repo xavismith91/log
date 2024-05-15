e28ba86399834e36caec9de05fcc2989f1ec80a6
Xavier Solano <xaviersolano91@gmail.com>
2024-05-15 16:21:43 -0600

    insert

---
+++ insertnewuser.php
@@ -0,0 +1,47 @@
+<?php
+//declaramos las variables 
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $usuario= $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+    $reconstrasena = $_POST['recontrasena'];
+    //con esta asignacion se encriptará la contraseña 
+    // $hash = password_hash($contrasena, PASSWORD_BCRYPT);
+//declaramos variables para la conexion a la base de datos
+
+$servername = "localhost"; 
+$database = "usuarios";
+$username = "root";
+$password = "";
+
+
+// creamos  el query para conexion de la base de datos 
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// revisamos la conexión
+
+if(!$conn)
+{
+    die("Falló la conexion: ". mysqli_connect_error()); 
+}else
+echo "Conexion exitosa";
+
+
+//hacemos el query para insertar registros 
+
+$sql = "INSERT INTO usuarios (nombre,correo,usuario,contrasena,recontrasena) VALUES ('$nombre','$correo','$usuario','$contrasena','$recontrasena')";
+
+if (mysqli_query($conn,$sql)){
+     
+     header("location:entrada.html");
+}
+else
+{
+   echo'<script type="text/javascript">
+        alert("No se pudo completar el registro");
+        window.location.href="register.php";
+        </script>';
+}
+mysqli_close($conn);
+?>
