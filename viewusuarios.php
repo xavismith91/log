566e2154ae1cda1a40598a6031f8418bd6c4a6ff
Xavier Solano <xaviersolano91@gmail.com>
2024-05-21 17:46:48 -0600

    viewusu

---
+++ viewusuarios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Usuario</title>
+</head>
+<body>
+
+    <h1>Lista de Usuarios Activos</h1>
+
+    <table>
+        <tr>
+            <th>nombre</th>
+            <th>correo</th>
+            <th>usuario</th>
+            <th>contrasena</th>
+        </tr>
+        
+    </table>
+    
+</body>
+</html>
