0147d20abc3bc6b21d8957cc8c20e551941e01d4
xavismith91 <xaviersolano91@gmail.com>
2024-05-24 17:22:24 -0600

    reboot

--- getusuarios.php
+++ getusuarios.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="css/style.css" rel="stylesheet"> -->
     <title>Vista Usuario</title>
 </head>
 
 <body>
 
-    <h1>Usuarios</h1>
-    
-    <table border="1">
+    <h1>Lista de Usuarios Activos</h1>
+
+    <table>
         <tr>
             <th>nombre</th>
             <th>correo</th>
@@ -25,9 +24,6 @@
 
 
 <?php
-
-session_start();
-
         $servername = "localhost";
         $username = "root";
         $password = "";
